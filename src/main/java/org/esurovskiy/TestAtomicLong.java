30125b5338176c672beb0eacdb42751e62d949cd
hacker13ua <hacker13ua@gmail.com>
2015-10-05 22:18:16 +0300

    remove comment code

--- src/main/java/org/esurovskiy/TestAtomicLong.java
+++ src/main/java/org/esurovskiy/TestAtomicLong.java
@@ -25,54 +25,15 @@ public class TestAtomicLong
 {
     AtomicLong value = new AtomicLong(0l);
 
-   /* public void test()
-    {*/
-//        final TestAtomicLong testAtomicLong = new TestAtomicLong();
-
-//        ForkJoinPool forkJoinPool = new ForkJoinPool();
-//        System.out.println("Parallelism=" + forkJoinPool.getParallelism());
-//        final List<ForkJoinTask<?>> tasks = new ArrayList<>();
-//        for(int i = 0; i < forkJoinPool.getParallelism(); i++)
-//        {
-//            tasks.add(forkJoinPool.submit(new Callable<Long>()
-//            {
-//                @Override
-//                public Long call() throws Exception
-//                {
-                    /*return*/ /*incrementLoop(value);*/
-//        System.out.println("Value=" + value);
-//                }
-//            }));
-//            System.out.println("Starting task №" + i);
-//        }
-       /* try
-        {
-            for(ForkJoinTask<?> task : tasks)
-            {
-                System.out.println("---value=" + task.get());
-
-            }
-            System.out.println("Value=" + value);
-        }*/
-       /* catch(Exception e)
-        {
-            e.printStackTrace();
-        }*/
-//    }
-
     @Benchmark
     @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public Long incrementAtomicLoop()
     {
-//        long loopStartTime = System.currentTimeMillis();
-//        long result = 0l;
         for(int i = 0; i < 1_000_000; i++)
         {
-            /*result = */value.incrementAndGet();
+            value.incrementAndGet();
         }
-//        System.out.println(Thread.currentThread().getName() + ": time=" + (System.currentTimeMillis() - loopStartTime));
-//        System.out.println(Thread.currentThread().getName() + ": value="+value.get());
         return value.get();
     }
 
