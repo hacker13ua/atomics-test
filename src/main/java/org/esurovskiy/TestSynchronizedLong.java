e575f1ec2121d3fa2603160f51c9cf1ea692c0d9
esurovskiy <hacker13ua@gmail.com>
2015-10-06 17:32:59 +0300

    add synchronized test

--- src/main/java/org/esurovskiy/TestSynchronizedLong.java
+++ src/main/java/org/esurovskiy/TestSynchronizedLong.java
@@ -7,33 +7,40 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * TODO: write JavaDoc
- *
  * @author Evgeniy Surovskiy
  */
-public class TestAtomicLong
+public class TestSynchronizedLong
 {
-    public static final int[] threadsCountArray = new int[]{ 16, };
-
+    public static final int[] threadsCountArray = new int[]{1, 2, 4, 8, 16, 32, 48, 64};
     @State(Scope.Benchmark)
-    public static class AtomicLongWrapper
+    public static class LongWrapper
     {
-        public AtomicLong value = new AtomicLong(0l);
+        Long value = 0l;
+        synchronized Long incrementAndGet()
+        {
+            return ++value;
+        }
+
+        Long get()
+        {
+            return value;
+        }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.SingleShotTime)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public Long incrementAtomicLoop(final AtomicLongWrapper atomicLongWrapper)
+    public Long incrementAtomicLoop(final LongWrapper longWrapper)
     {
         for(int i = 0; i < 1_000_000; i++)
         {
-            atomicLongWrapper.value.incrementAndGet();
+            longWrapper.incrementAndGet();
         }
-        return atomicLongWrapper.value.get();
+        System.out.println("Value="+longWrapper.get());
+        return longWrapper.get();
     }
 
     public static void main(String[] args) throws RunnerException
@@ -41,7 +48,7 @@ public class TestAtomicLong
         for(final int threadsCount : threadsCountArray)
         {
             Options opt = new OptionsBuilder()
-                    .include(TestAtomicLong.class.getSimpleName())
+                    .include(TestSynchronizedLong.class.getSimpleName())
                     .jvmArgs("-server", "-Xmx2G")
                     .warmupIterations(10)
                     .measurementIterations(25)
